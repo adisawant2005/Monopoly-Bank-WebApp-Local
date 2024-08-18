dfdec118a49d60c1af0dad0345f951f8f4bf391d
Aditya Raju Sawant <adisawant454@gmail.com>
2024-08-18 09:24:42 +0530

    added files from dev files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-purple-800 text-white p-2 w-full h-16">
+      <span className="text-center text-3xl block">
+        <span>
+          “Where a trust becomes a monopoly, the state has an immediate right to
+          interfere.”
+        </span>{" "}
+        - Theodore Roosevelt
+      </span>
+    </div>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BankComponent from "./BankComponent";
+
+export default function Main() {
+  return (
+    <div className="bg-sky-600 text-white w-full p-8">
+      <BankComponent />
+    </div>
+  );
+}
